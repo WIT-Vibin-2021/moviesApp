a4800c8496063f70bf5aa4f754a20a14262fa25e
Vibin Varghese <mailme.vibin@gmail.com>
2022-02-28 18:37:18 +0000

    Movie Details  page components.

---
+++ src/components/movieDetails/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Chip from "@material-ui/core/Chip";
+import Paper from "@material-ui/core/Paper";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import MonetizationIcon from "@material-ui/icons/MonetizationOn";
+import StarRate from "@material-ui/icons/StarRate";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  chipRoot: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    listStyle: "none",
+    padding: theme.spacing(1.5),
+    margin: 0,
+  },
+  chipSet: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    listStyle: "none",
+    padding: theme.spacing(1.5),
+    margin: 0,
+  },
+  chipLabel: {
+    margin: theme.spacing(0.5),
+  },
+}));
+
+const MovieDetails = ( props) => {
+  const classes = useStyles();
+  const movie = props.movie
+
+  return (
+    <>
+      <Typography variant="h5" component="h3">
+        Overview
+      </Typography>
+
+      <Typography variant="h6" component="p">
+        {movie.overview}
+      </Typography>
+      <div className={classes.chipRoot}>
+      <Paper component="ul" className={classes.chipSet}>
+        <li>
+          <Chip label="Genres" className={classes.chipLabel} color="primary" />
+        </li>
+        {movie.genres.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} className={classes.chip} />
+          </li>
+        ))}
+      </Paper>
+      <Paper component="ul" className={classes.chipSet}>
+        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
+        <Chip
+          icon={<MonetizationIcon />}
+          label={`${movie.revenue.toLocaleString()}`}
+        />
+        <Chip
+          icon={<StarRate />}
+          label={`${movie.vote_average} (${movie.vote_count}`}
+        />
+        <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+      </div>
+      </>
+  );
+};
+export default  MovieDetails ;
