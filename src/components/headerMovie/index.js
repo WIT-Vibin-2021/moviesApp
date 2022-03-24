102841dae13d5175a3bb1ecad6cc314cb5e30e5b
Vibin Varghese <91533254+WIT-Vibin-2021@users.noreply.github.com>
2022-03-24 10:39:32 +0000

    Fix backward/foeward arrows

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -6,6 +6,7 @@ import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import HomeIcon from "@material-ui/icons/Home";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,16 +21,15 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const MovieHeader = (props) => {
+const MovieHeader = ({ movie }) => {
   const classes = useStyles();
-  const movie = props.movie;
+  const history = useHistory();
 
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back">
+      <IconButton aria-label="go back" onClick={() => history.goBack()}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
       <Typography variant="h4" component="h3">
         {movie.title}
         <a href={movie.homepage}>
@@ -38,7 +38,7 @@ const MovieHeader = (props) => {
         <br />
         <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
+      <IconButton aria-label="go forward" onClick={() => history.goForward()}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
--- src/components/headerMovieList/index.js
+++ src/components/headerMovieList/index.js
@@ -5,6 +5,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import { useHistory } from 'react-router-dom'
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,20 +21,17 @@ const useStyles = makeStyles((theme) => ({
 const Header = (props ) => {
   const classes = useStyles();
   const title = props.title
+  const history = useHistory(); 
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton
-        aria-label="go back"
-      >
+      <IconButton aria-label="go back" onClick={() => history.goBack()}>
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
       <Typography variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
+      <IconButton aria-label="go forward" onClick={() => history.goForward()}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
